21840cf0edbcdf5ae9bbbba0b998df8cbbdf5376
git config --global user.name <saeid.moradi.dev@gmail.com>
2025-11-02 02:38:22 -0800

    Add Navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <h1 className="bg-amber-500 px-5 text-4xl rounded-xl font-light">Saeid</h1>
+      <h1 className="bg-amber-500 px-5 text-4xl rounded-xl font-light">Saeid Moradi</h1>
     </div>
   );
 }
