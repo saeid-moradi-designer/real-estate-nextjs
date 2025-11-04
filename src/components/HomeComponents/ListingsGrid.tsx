a56ecb0175ba369b9a6eff557b32d83ece121fb6
git config --global user.name <saeid.moradi.dev@gmail.com>
2025-11-04 08:16:27 -0800

    add pb-4 for listing grid

--- src/components/HomeComponents/ListingsGrid.tsx
+++ src/components/HomeComponents/ListingsGrid.tsx
@@ -17,7 +17,7 @@ interface ListingsGridProps {
 
 const ListingsGrid: React.FC<ListingsGridProps> = ({ listings }) => {
   return (
-    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full px-4">
+    <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 w-full px-4 pb-4">
       {listings.map((listing) => (
         <div
           key={listing.id}
